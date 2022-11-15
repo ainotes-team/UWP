bbfa3a6bbda597c9ef318ff60a88d7aecffb7c97
HDhotdog <o.laraia@web.de>
2022-11-15 23:08:50 +0100

    Fix custom pen color picker #7

--- AINotes/Controls/Input/CustomColorPicker.xaml.cs
+++ AINotes/Controls/Input/CustomColorPicker.xaml.cs
@@ -12,6 +12,7 @@ using Helpers.Extensions;
 namespace AINotes.Controls.Input {
     public partial class CustomColorPicker {
         public const int SwatchSize = 26;
+        private const int DefaultColorMax = 21;
         
         public ArrayList DefaultColors = new ArrayList {
             "#FFC114",
@@ -34,6 +35,8 @@ namespace AINotes.Controls.Input {
             "#FFFFFF",
         };
 
+        public static ArrayList CustomColors = new ArrayList();
+
         public event Action<Color> ColorSelected;
 
         public CustomColorPicker(string selectHexColor = null, bool showPlus = false) {
@@ -53,6 +56,7 @@ namespace AINotes.Controls.Input {
 
             var top = 0;
             var left = 0;
+            DefaultColors.AddRange(CustomColors);
             foreach (string hexColor in DefaultColors) {
                 var color = ColorCreator.FromHex(hexColor);
                 var frame = new CustomFrame {
@@ -72,6 +76,7 @@ namespace AINotes.Controls.Input {
                 if (hexColor == selectHexColor) {
                     frame.BorderThickness = new Thickness(3);
                 }
+
                 
                 this.AddChild(frame, top, left);
 
@@ -101,9 +106,17 @@ namespace AINotes.Controls.Input {
         }
 
         private void OnPlusFrameTouch(object o, WTouchEventArgs args) {
-            // TODO: Add Color Code
             if (args.ActionType != WTouchAction.Pressed) return;
-            new MDContentPopup("Add Color", new ColorPicker()).Show();
+            ColorPicker colorPicker = new ColorPicker();
+            var popup = new MDContentPopup("Add Color", colorPicker, () => {
+                Color color = colorPicker.Color;
+                if(CustomColors.Count >= DefaultColorMax) {
+                    CustomColors.RemoveAt(0);
+                }
+                CustomColors.Add(color.ToHex());
+                ColorSelected?.Invoke(color);
+            });
+            popup.Show();
         }
     }
 }
